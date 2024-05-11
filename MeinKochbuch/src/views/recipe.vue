0cdec4e9a41e826fd917052bc05c468712d692bc
Leif <leifstoehr333@gmail.com>
2024-05-11 18:38:02 +0200

    recipe v-if

--- MeinKochbuch/src/views/recipe.vue
+++ MeinKochbuch/src/views/recipe.vue
@@ -1,26 +1,32 @@
 <script setup>
-
+let showRecipeName = true;
 </script>
 
 <template>
   <div class="recipeheader">
     <h1>Rezepte</h1>
   </div>
-  <div class="recipename">
-    <h2>Spaghetti Carbonara</h2>
-  </div>
-    <div class="recipedescription">
+  <div class="recipename" v-if="showRecipeName">
+    <h2>Spaghetti Carbonara</h2> <br>
       <p>Spaghetti Carbonara ist ein klassisches italienisches Nudelgericht, das in der Regel aus Spaghetti, Guanciale, Ei, Pecorino Romano und Pfeffer zubereitet wird. Die Zutaten werden in einer Pfanne gemischt, bis sie eine cremige Konsistenz haben. Das Gericht wird oft mit zusätzlichem Pecorino Romano und Pfeffer garniert.</p>
-    </div>
-    <div class="recipeimage">
-      <img src="https://images.unsplash.com/photo-1606780840483-4b0b7e3f3b3b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" width="500" height="600" alt="Spaghetti Carbonara">
-    </div>
-    <div class="recipetime">
-      <p> Zubereitungszeit: 20 Minuten</p> <br>
-        <p> Kochzeit: 15 Minuten</p>
+      <br>
+  </div>
+    <div>
+        <h3>Zubereitung</h3>
+        <p>1. Die Spaghetti in einem großen Topf mit kochendem Salzwasser al dente kochen.</p>
+        <p>2. In der Zwischenzeit das Guanciale in einer Pfanne bei mittlerer Hitze knusprig braten.</p>
+        <p>3. Die Eier in einer Schüssel verquirlen und den geriebenen Pecorino Romano hinzufügen.</p>
+        <p>4. Die gekochten Spaghetti abgießen und in die Pfanne mit dem Guanciale geben. Die Ei-Käse-Mischung darüber gießen und gut vermengen.</p>
+        <p>5. Mit Pfeffer würzen und sofort servieren.</p>
     </div>
     <div class="recipeingredients">
-      <h3>Zutaten:</h3>
+      <h3>Dauer</h3>
+        <ul>
+            <li> Zubereitungszeit: 20 Minuten</li>
+            <li> Kochzeit: 15 Minuten</li>
+        </ul>
+
+      <h3>Zutaten</h3>
       <ul>
         <li>400g Spaghetti</li>
         <li>200g Guanciale</li>
@@ -29,6 +35,7 @@
         <li>Pfeffer</li>
       </ul>
     </div>
+
 </template>
 
 <style>
