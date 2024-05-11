e6e6670e27db781bc3aa0713c051d4c9a836dd7e
Leif <leifstoehr333@gmail.com>
2024-05-11 17:53:13 +0200

    recipe test1

--- MeinKochbuch/src/views/recipe.vue
+++ MeinKochbuch/src/views/recipe.vue
@@ -3,9 +3,19 @@
 </script>
 
 <template>
-  <div class="recipe">
-    <h1>This is a recipe page</h1>
+  <div class="recipeheader">
+    <h1>Rezepte</h1>
   </div>
+  <div class="recipename">
+    <h2>Spaghetti Carbonara</h2>
+  </div>
+  <div class="recipe">
+    <div class="recipetext">
+      <p>Spaghetti Carbonara ist ein klassisches italienisches Nudelgericht, das in der Regel aus Spaghetti, Guanciale, Ei, Pecorino Romano und Pfeffer zubereitet wird. Die Zutaten werden in einer Pfanne gemischt, bis sie eine cremige Konsistenz haben. Das Gericht wird oft mit zusätzlichem Pecorino Romano und Pfeffer garniert.</p>
+    </div>
+    <div class="recipeimage">
+      <img src="https://images.unsplash.com/photo-1606780840483-4b0b7e3f3b3b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="Spaghetti Carbonara">
+    </div>
 </template>
 
 <style>
