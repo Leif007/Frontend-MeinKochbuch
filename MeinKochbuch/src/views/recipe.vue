8d6bb49721f745539f3713a1d408704911a1dbdc
Leif <leifstoehr333@gmail.com>
2024-05-11 18:05:07 +0200

    recipe add1

--- MeinKochbuch/src/views/recipe.vue
+++ MeinKochbuch/src/views/recipe.vue
@@ -9,11 +9,25 @@
   <div class="recipename">
     <h2>Spaghetti Carbonara</h2>
   </div>
-    <div class="recipetext">
+    <div class="recipedescription">
       <p>Spaghetti Carbonara ist ein klassisches italienisches Nudelgericht, das in der Regel aus Spaghetti, Guanciale, Ei, Pecorino Romano und Pfeffer zubereitet wird. Die Zutaten werden in einer Pfanne gemischt, bis sie eine cremige Konsistenz haben. Das Gericht wird oft mit zusätzlichem Pecorino Romano und Pfeffer garniert.</p>
     </div>
     <div class="recipeimage">
-      <img src="https://images.unsplash.com/photo-1606780840483-4b0b7e3f3b3b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="Spaghetti Carbonara">
+      <img src="https://images.unsplash.com/photo-1606780840483-4b0b7e3f3b3b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" width="500" height="600" alt="Spaghetti Carbonara">
+    </div>
+    <div class="recipetime">
+      <p> Zubereitungszeit: 20 Minuten</p> <br>
+        <p> Kochzeit: 15 Minuten</p>
+    </div>
+    <div class="recipeingredients">
+      <h3>Zutaten:</h3>
+      <ul>
+        <li>400g Spaghetti</li>
+        <li>200g Guanciale</li>
+        <li>4 Eier</li>
+        <li>100g Pecorino Romano</li>
+        <li>Pfeffer</li>
+      </ul>
     </div>
 </template>
 
