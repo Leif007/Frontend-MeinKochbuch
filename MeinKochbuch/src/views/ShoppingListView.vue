64716124efbae29cf82216ccede0d68ee9fcd90e
KingChaos16 <smpcraft12345@gmail.com>
2024-06-16 14:49:17 +0200

    updated the generate shopping list

--- MeinKochbuch/src/views/ShoppingListView.vue
+++ MeinKochbuch/src/views/ShoppingListView.vue
@@ -11,7 +11,7 @@ const shoppingList = ref([]);
 watchEffect(() => {
   const newShoppingList = props.shoppingList;
   if (newShoppingList) {
-    shoppingList.value = JSON.parse(newShoppingList);
+    shoppingList.value = JSON.parse(newShoppingList).map(item => ({ name: item, selected: false }));
   } else {
     shoppingList.value = [];
   }
@@ -27,7 +27,10 @@ watchEffect(() => {
   <div class="shoppingList">
     <h1>Shopping List</h1>
     <ul>
-      <li v-for="item in shoppingList" :key="item">{{ item }}</li>
+      <li v-for="item in shoppingList" :key="item.name">
+        <input type="checkbox" v-model="item.selected">
+        <span :class="{ 'item-selected': item.selected }">{{ item.name }}</span>
+      </li>
     </ul>
   </div>
 </template>
@@ -38,6 +41,10 @@ watchEffect(() => {
   padding: 1rem;
   border: 1px solid var(--color-border);
   border-radius: 5px;
+}
+
+.item-selected {
+  text-decoration: line-through;
 }
 
 </style>
