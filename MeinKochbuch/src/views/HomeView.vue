7985081314c5e793890d04e11f0a3fd8801a34fc
Cirrous <christianraisch@gmail.com>
2024-06-26 21:32:31 +0200

    changed styling

--- MeinKochbuch/src/views/HomeView.vue
+++ MeinKochbuch/src/views/HomeView.vue
@@ -1,31 +1,15 @@
 <script setup>
 
+import Welcome from "@/components/Welcome.vue";
 </script>
 
 <template>
   <header>
     <div class="home">
-    <h1>Dein Kochbuch</h1>
+      <Welcome />
     </div>
   </header>
 </template>
 <style>
-@media (min-width: 1024px) {
-  .home {
-    min-height: 75vh;
-    display: flex;
-    align-items: center;
-    padding: 200px;
-  }
-  .beschreibung {
-    margin-top: -30rem;
-    margin-left: 1rem;
-    font-size: large;
-    overflow: auto;
-    display: flex;
-    align-items: center;
-    background-blend-mode: color;
 
-  }
-}
 </style>
