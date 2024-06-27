07c7b03cf7ae6f6e8a9d8a84696e6f78d5e6c05b
KingChaos16 <smpcraft12345@gmail.com>
2024-06-27 15:53:06 +0200

    Fat Secret API v10

--- MeinKochbuch/src/components/api.js
+++ MeinKochbuch/src/components/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const url = import.meta.env.VITE_APP_BACKEND_BASE_URL
+const url = import.meta.env.VITE_BACKEND_URL
 
 const apiClient = axios.create({
     baseURL: url,
@@ -24,18 +24,25 @@ export default {
         return apiClient.get('/recipes')
     },
     searchAndGetDetails(foodNames) {
-    const backendUrl = 'https://meinkochbuch-backend.onrender.com';
+        const backendUrl = 'https://meinkochbuch-backend.onrender.com';
         console.log(`Fetching details for: ${foodNames}`);
-    fetch(`${backendUrl}/searchAndGetDetails/${foodNames}`)
-      .then(response => response.json())
-      .then(data => {
-          console.log(`Received data: ${JSON.stringify(data)}`);
-          console.log(data);
-      })
-      .catch(error => {
-          console.error('Error:', error);
-      });
-}
+        return fetch(`${backendUrl}/searchAndGetDetails/${foodNames}`)
+          .then(response => {
+              if (!response.ok) {
+                  throw new Error(`HTTP error! status: ${response.status}`);
+              }
+              return response.json();
+          })
+          .then(data => {
+              console.log(`Received data: ${JSON.stringify(data)}`);
+              console.log(data);
+              return data;
+          })
+          .catch(error => {
+              console.error('Error:', error);
+          });
+    }
+
 }
 
 
